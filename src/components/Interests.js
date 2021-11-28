4d7461f0218ef2b78ef62ee91cc8d5aab87f5b68
Shirtless Coder <shirtlesscoder@gmail.com>
2021-11-28 15:58:13 +0700

    Updated commit

---
+++ src/components/Interests.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Interests() {
+    return(
+        <div className="interests--container">
+            <h3 className="interests--title">Interests</h3>
+            <p className="interests--text">Food enthusiast. Music lover. Travel geek. Entrepreneur. Reader. Outdoor sports fanatic.</p>
+        </div>
+    )
+}
